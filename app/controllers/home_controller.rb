623beea6c592e9476411653e9bcd7e4afa078c3a
Dustin G <Dustin.Guillen14@yahoo.com>
2024-06-11 21:47:24 -0600

    [ticket-number] added flash messages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
 
     def index
-        
+        flash[:notice] = "Logged in successfully"
+        flash[:alert] = "Invalid email or password"
     end
 
 end
